17a232299560b1d8f8eac7d790eb57377d1a5b20
abbasnadir <abbasnadir12345@gmail.com>
2026-04-23 02:26:40 +0530

    Added prod url as cors origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,9 +20,8 @@ const __dirname = import.meta.dirname;
 
 const devOrigins = new Set([
   "http://localhost:3000",
-  "http://127.0.0.1:3000",
   "http://localhost:3001",
-  "http://127.0.0.1:3001",
+  process.env.PROD_URL,
 ]);
 
 function isAllowedOrigin(origin: string | undefined) {
